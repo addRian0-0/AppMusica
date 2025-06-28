f17128942b109e16f6b7983889d4fac2037a3da6
JheromScript <63478957+JheromScript@users.noreply.github.com>
2025-06-28 17:17:53 -0600

    Add auto-stop timer to `Reproducir` for automatic playback pause
    - Introduced a `javax.swing.Timer` to automatically pause playback after 1 minute.
    - Updated `Reproducir` logic to stop existing timers during playback reset.

--- src/main/java/Reproducir/Reproducir.java
+++ src/main/java/Reproducir/Reproducir.java
@@ -7,12 +7,17 @@ import javazoom.jl.player.Player;
 
 public class Reproducir {
     private static Player player;
+    private static javax.swing.Timer autoStopTimer;
 
     public static void Reproducir(String urlStr) {
         try {
             if (player != null) {
                 player.close();
             }
+            if (autoStopTimer != null) {
+                autoStopTimer.stop();
+            }
+
             URL url = new URL(urlStr);
             BufferedInputStream bis = new BufferedInputStream(url.openStream());
             player = new Player(bis);
@@ -23,6 +28,14 @@ public class Reproducir {
                     System.err.println("Error al reproducir: " + e.getMessage());
                 }
             }).start();
+
+            //timer para detener la cancion en 1 min
+            autoStopTimer = new javax.swing.Timer(60000, evt -> {
+                pausar();
+                autoStopTimer.stop();
+            });
+            autoStopTimer.start();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
